ea781a8bccde0aeb575bfe95623f3a6010d6ad91
lhirima <fabrigaslhi@gmail.com>
2026-08-07 17:20:06 +0800

    Add files via upload

---
+++ FabrigasTICTACTOE/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace FabrigasTICTACTOE
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(65, 37);
+            label1.Name = "label1";
+            label1.Size = new Size(112, 40);
+            label1.TabIndex = 0;
+            label1.Text = "Player:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(65, 77);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 40);
+            label2.TabIndex = 1;
+            label2.Text = "Cpu:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(180, 77);
+            label3.Name = "label3";
+            label3.Size = new Size(34, 40);
+            label3.TabIndex = 3;
+            label3.Text = "0";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(180, 37);
+            label4.Name = "label4";
+            label4.Size = new Size(34, 40);
+            label4.TabIndex = 2;
+            label4.Text = "0";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(128, 128, 255);
+            button1.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(72, 125);
+            button1.Name = "button1";
+            button1.Size = new Size(84, 78);
+            button1.TabIndex = 4;
+            button1.Text = "O";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(128, 128, 255);
+            button2.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(72, 209);
+            button2.Name = "button2";
+            button2.Size = new Size(84, 78);
+            button2.TabIndex = 5;
+            button2.Text = "O";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(128, 128, 255);
+            button3.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(72, 293);
+            button3.Name = "button3";
+            button3.Size = new Size(84, 78);
+            button3.TabIndex = 6;
+            button3.Text = "O";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(128, 128, 255);
+            button4.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(162, 293);
+            button4.Name = "button4";
+            button4.Size = new Size(84, 78);
+            button4.TabIndex = 9;
+            button4.Text = "O";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(128, 128, 255);
+            button5.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.Location = new Point(162, 209);
+            button5.Name = "button5";
+            button5.Size = new Size(84, 78);
+            button5.TabIndex = 8;
+            button5.Text = "O";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(128, 128, 255);
+            button6.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.Location = new Point(162, 125);
+            button6.Name = "button6";
+            button6.Size = new Size(84, 78);
+            button6.TabIndex = 7;
+            button6.Text = "O";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(128, 128, 255);
+            button7.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.Location = new Point(252, 293);
+            button7.Name = "button7";
+            button7.Size = new Size(84, 78);
+            button7.TabIndex = 12;
+            button7.Text = "O";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(128, 128, 255);
+            button8.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.Location = new Point(252, 209);
+            button8.Name = "button8";
+            button8.Size = new Size(84, 78);
+            button8.TabIndex = 11;
+            button8.Text = "O";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.FromArgb(128, 128, 255);
+            button9.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button9.Location = new Point(252, 125);
+            button9.Name = "button9";
+            button9.Size = new Size(84, 78);
+            button9.TabIndex = 10;
+            button9.Text = "O";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 255);
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(405, 425);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+    }
+}
